2de3968078f7d587e43ff0ea7b1e51a2caf04bca
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-13 10:25:28 -0500

    update dbcontext & migrations
    - Updated ApplicationDbContext
    - Added new Db Migrations

--- FluentBlazorAuthTest/Data/ApplicationDbContext.cs
+++ FluentBlazorAuthTest/Data/ApplicationDbContext.cs
@@ -7,5 +7,25 @@ namespace FluentBlazorAuthTest.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<FluentBlazorAuthTest.Data.Models.Space> Space { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Model configuration for Space
+            modelBuilder.Entity<Space>(entity =>
+            {
+                // Setting precision for Latitude
+                entity.Property(e => e.Latitude).HasPrecision(9, 6);
+
+                // Setting precision for Longitude
+                entity.Property(e => e.Longitude).HasPrecision(9, 6);
+
+                // Additional configurations for Space can be added here
+            });
+
+            // Other model configurations can be added here
+        }
+
     }
 }
---
+++ FluentBlazorAuthTest/Data/Migrations/20231213043340_UpdateSpace00.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluentBlazorAuthTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSpace00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
