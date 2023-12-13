d277abf421c8d8a92f4bedaae67f515ec1ad403a
Jonathan Aaronson <112821604+yaaronson@users.noreply.github.com>
2023-12-12 23:39:33 -0500

    Footer menu added

--- FluentBlazorAuthTest/wwwroot/JS/Darkmode.js
+++ FluentBlazorAuthTest/wwwroot/JS/Darkmode.js
@@ -1,19 +1,9 @@
-﻿// Dark Mode 
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-
-    // Save user preference in local storage
-    const isDarkMode = body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', isDarkMode);
+﻿window.applyTheme = function (theme) {
+    console.log('Applying theme:', theme);
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
 }
 
-// Load dark mode preference from local storage
-document.addEventListener('DOMContentLoaded', () => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    const body = document.body;
-
-    if (isDarkMode) {
-        body.classList.add('dark-mode');
-    }
-});
