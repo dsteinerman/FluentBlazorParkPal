c27752f41530fa3889244a5c3682db42391b0da4
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-24 19:19:13 -0500

    Create ISpaceService.cs

---
+++ FluentBlazorAuthTest/Data/Services/ISpaceService.cs
@@ -0,0 +1,24 @@
+﻿namespace FluentBlazorAuthTest.Data.Services;
+
+public interface ISpaceService
+{
+    /* Create */
+
+    Task<Space> CreateSpaceAsync(Space space);
+
+    /* Read */
+
+    Task<Space> GetSpaceByIdAsync(string spaceId);
+
+    // Can narrow down search criteria by invoking LINQ queries
+    Task<IEnumerable<Space>> GetAllSpacesAsync();
+
+    /* Update */
+
+    Task UpdateSpaceAsync(Space space);
+
+    /* Delete */
+
+    Task DeleteSpaceAsync(string spaceId);
+
+}
