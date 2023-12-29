596f75ae913e409fab65db49eadd759f218d72c5
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-28 20:59:14 -0500

    Added initial scaffolding for IDBContextFactory Injection
    - Implemented the use of the Factory Pattern to implement

--- FluentBlazorAuthTest/Program.cs
+++ FluentBlazorAuthTest/Program.cs
@@ -30,6 +30,10 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
