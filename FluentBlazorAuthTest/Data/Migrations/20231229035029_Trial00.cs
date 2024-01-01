b7977523b263fde7278c57c8a99b370f367735ae
Steven Maltz <41177179+vcr-shop@users.noreply.github.com>
2023-12-31 19:52:54 -0500

    Update Db Migration Files

---
+++ FluentBlazorAuthTest/Data/Migrations/20231229035029_Trial00.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluentBlazorAuthTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Trial00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Spaces",
+                type: "decimal(8,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Spaces",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVacant",
+                table: "Spaces",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Spaces");
+
+            migrationBuilder.DropColumn(
+                name: "IsVacant",
+                table: "Spaces");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Spaces",
+                type: "decimal(8,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)",
+                oldNullable: true);
+        }
+    }
+}
