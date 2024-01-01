59e1e4addd1436cd13e4aeb288afe7118e57bf4a
Jonathan Aaronson <112821604+yaaronson@users.noreply.github.com>
2024-01-01 16:01:20 -0500

    fixed redirects

---
+++ FluentBlazorAuthTest/Data/Migrations/20231228003959_addIsAvailable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluentBlazorAuthTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsAvailable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Spaces",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Spaces");
+        }
+    }
+}
